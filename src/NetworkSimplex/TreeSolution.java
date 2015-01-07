71dea9f68fa1cd9828cfe211a77d707e84df313c
unknown <IG@nbookig2.novatec-gmbh.lan>
2015-01-07 20:14:11 +0100

    implemented the method find candidates which helps to find the entering arc

--- src/NetworkSimplex/TreeSolution.java
+++ src/NetworkSimplex/TreeSolution.java
@@ -1,6 +1,7 @@
 package NetworkSimplex;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class TreeSolution {
 
@@ -15,11 +16,11 @@ public class TreeSolution {
 
 	private Arc[] U; //the partition where flow equals upper cap
 	private ArrayList<Arc2>  U2;
-	
+
 	private Arc[] Tree; //the arcs in the tree
 	private ArrayList<Arc2> Tree2 = new ArrayList<Arc2>();
-	
-//	private ArrayList<Double> fairPrices = new ArrayList<Double>(); //the costs in the nodes
+
+	//	private ArrayList<Double> fairPrices = new ArrayList<Double>(); //the costs in the nodes
 	private double[] fairPrices;
 
 
@@ -36,22 +37,22 @@ public class TreeSolution {
 
 	public TreeSolution(ArrayList<Arc2>  L2, Node[] nodes, int numberOfNodes, double maxCost) {
 		this.L2 = L2;
-//		int kIndex = numberOfNodes +1;	//index of the artificial node
+		//		int kIndex = numberOfNodes +1;	//index of the artificial node
 		int kIndex = 0;
 		double costArtificialArc = 1 + 0.5 * numberOfNodes* maxCost;	//->skript
-		
+
 		this.predecessorArray = new int[numberOfNodes+1];
 		predecessorArray[kIndex] = -1;
-		
+
 		this.depthArray = new int[numberOfNodes+1];
 		depthArray[kIndex]= 0;
-		
+
 		this.thread = new int[numberOfNodes +1];
 		thread[kIndex] = numberOfNodes; //richtig initialisiert?
-		
+
 		this.fairPrices = new double[numberOfNodes+1];
 		fairPrices[kIndex] = 0; //this is the one that choose arbritrariliy (n variables, n-1 equations)
-		
+
 		int startNodeIndex;
 		int endNodeIndex;
 		for(int i = 1; i<nodes.length; i++){ //start at one bcz at index 0 there is no node to keep it easy i.e. nodeIndex = arrayIndex
@@ -72,7 +73,10 @@ public class TreeSolution {
 			this.depthArray[i] = 1;
 			this.thread[i] = kIndex;
 		}
-		
+		LinkedList<Arc2> candidates = findCandidatesForEnteringArc(true, 10);
+		System.out.println("candidates:");
+		System.out.println(candidates);
+
 	}
 
 	public void setPredecessorArray(int[] predecessorArray) {
@@ -110,6 +114,33 @@ public class TreeSolution {
 
 	}
 
+	/**
+	 * 
+	 * @param firstRun
+	 * @param r
+	 * @return
+	 */
+	LinkedList<Arc2> findCandidatesForEnteringArc(boolean firstRun,int r) { //r is the number of candidates that we ll provide in this call
+		LinkedList<Arc2> candidates = new LinkedList<Arc2>();
+		int i =0;
+		for(Arc2 arc : L2){
+			int startnode;
+			int endnode;
+			if(firstRun) {
+				startnode = arc.getStartNodeIndex();
+				endnode = arc.getEndNodeIndex();
+				arc.setReducedCosts(arc.getCost() + fairPrices[startnode] - fairPrices[endnode]);
+			}
+			if(arc.getReducedCosts()<0){
+				candidates.add(arc);
+				i++;
+			}
+			if(i==r) break;
+		}
+		return candidates;
+
+	}
+
 	private String intArrayToString(int[] array){
 		StringBuffer string = new StringBuffer();
 		for(int i = 0; i<array.length; i++) {
@@ -120,25 +151,25 @@ public class TreeSolution {
 		}
 		return string.toString();
 	}
-	
-	
+
+
 	public String toString() {
 		StringBuffer string = new StringBuffer("Predecessor Array: ");
 		string.append(intArrayToString(predecessorArray));
-		
+
 		string.append("\ndepth Array: ");
 		string.append(intArrayToString(depthArray));
-		
+
 		string.append("\nthread Array: ");
 		string.append(intArrayToString(thread));
-		
-		
+
+
 		string.append("\nL2: ");
 		string.append(L2);
-		
+
 		string.append("\nTree2: ");
 		string.append(Tree2);
-		
+
 		return string.toString();
 	}
 
