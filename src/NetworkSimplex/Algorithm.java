88c3d752da1ba4d865dbaedd3f61d0bde0764cd3
unknown <IG@nbookig2.novatec-gmbh.lan>
2015-01-17 12:13:38 +0100

    nothing

--- src/NetworkSimplex/Algorithm.java
+++ src/NetworkSimplex/Algorithm.java
@@ -7,6 +7,7 @@ public class Algorithm {
 	
 	public static void main(String[] args) {
 		try {
+			long startTime = System.currentTimeMillis();
 			Reader reader = new Reader(args[0]);
 			TreeSolution tree = reader.getTreeSolution();
 			int numberOfITeration=0;
@@ -15,10 +16,12 @@ public class Algorithm {
 				System.out.println("\n\n\n\n\n iteration:");
 				System.out.println(numberOfITeration);
 			};
+			long endTime = System.currentTimeMillis();
 			System.out.println(tree);
 			System.out.println("\nthe costs of this soultion are:");
 			System.out.println("the way the costs are calculated might be wrong, so dont really rely on that. method was written late at night:)");
 			System.out.println(tree.getCosts());
+			System.out.println("That took " + (endTime - startTime) + " milliseconds");
 			//maybe write the solution now to a file or so
 			
 //			System.out.println("\033[0;0mhallo\033[0;1m");
@@ -31,3 +34,4 @@ public class Algorithm {
 	}
 
 }
+
