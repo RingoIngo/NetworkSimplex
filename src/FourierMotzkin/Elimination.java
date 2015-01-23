c48c180df63693b93c83f2e9e852fdac0e5cf00f
daniel <d.eggebrecht@gmx.net>
2015-01-23 18:38:42 +0100

    add constructor and getter method to elimination class add reader code

--- src/FourierMotzkin/Elimination.java
+++ src/FourierMotzkin/Elimination.java
@@ -2,61 +2,90 @@ package FourierMotzkin;
 
 import java.util.LinkedList;
 
-
 public class Elimination {
-	
+
 	private LinkedList<Integer> P;
-	
+
 	private LinkedList<Integer> N;
-	
+
 	private LinkedList<Integer> Z;
-	
+
+	// an array with all variables we have to eliminate
+	private int[] eliminationVariables;
+
+	// the matrix with all coefficients of the conditions and the vector b in
+	// the last column.
+	// to keep it easy, the first column and the first row is 0
+	private int[][] matrixA;
+
+	/**
+	 * This contructor needed informations the Reader read in out of the
+	 * inputfile. This is the coefficientsmatrix with vector b in it and the
+	 * variables we have to eliminate
+	 */
+	public Elimination(int[][] matrixA, int[] eliminationVariables) {
+		this.eliminationVariables = eliminationVariables;
+		this.matrixA = matrixA;
+	}
+
+	/**
+	 * getter method for elimination variables
+	 * @return eliminationVariables
+	 */
+	public int[] getEliminationVariables() {
+		return this.eliminationVariables;
+	}
+
+	/**
+	 * getter method for matrix A
+	 * @return	matrixA
+	 */
+	public int[][] getMatrixA() {
+		return this.matrixA;
+	}
+
 	/**
 	 * Zuordnung der Gleichungen zu P, N, Z
 	 */
-	public void assign (double[][] system, int elVar) {
-		int i=1;
+	public void assign(double[][] system, int elVar) {
+		int i = 1;
 		while (i < system.length) {
-			if (system[i][elVar]<0){
+			if (system[i][elVar] < 0) {
 				N.add(i);
-			}
-			else if (system[i][elVar]>0){
+			} else if (system[i][elVar] > 0) {
 				P.add(i);
-			}
-			else {
+			} else {
 				Z.add(i);
 			}
 		}
-		
+
 	}
-	/** 
-	 *  Skaliert die Zeilen der Matrix, sd in der Spalte der elVar nur -1,0,1 steht
+
+	/**
+	 * Skaliert die Zeilen der Matrix, sd in der Spalte der elVar nur -1,0,1
+	 * steht
 	 */
-	public double[][] scale (double[][] system, int elVar) {
-		double [][] scaleSystem= new double [system.length][system[1].length];
-		
-		for (int i=0;i < system.length; i++) {
-				for(int j=0; j< system[1].length; j++){
-					scaleSystem[i][j] = system[i][j]/system[i][elVar];
-				}
+	public double[][] scale(double[][] system, int elVar) {
+		double[][] scaleSystem = new double[system.length][system[1].length];
+
+		for (int i = 0; i < system.length; i++) {
+			for (int j = 0; j < system[1].length; j++) {
+				scaleSystem[i][j] = system[i][j] / system[i][elVar];
+			}
 		}
 		return scaleSystem;
 	}
-	
-	
-	
+
 	/**
 	 * 
-	 * @param system Matrix der Ungleichungen
-	 * @param elVar zu eliminierende Variable
+	 * @param system
+	 *            Matrix der Ungleichungen
+	 * @param elVar
+	 *            zu eliminierende Variable
 	 * @return
 	 */
-	
-	
+
 	public void eliminate ( double[][] scaleSystem, int elVar) {
-		double [][] solution = new double [][scaleSystem[1]]
+//		double [][] solution = new double [][scaleSystem[1]]
 	}
-	
-	
-	
 }
--- src/FourierMotzkin/Reader.java
+++ src/FourierMotzkin/Reader.java
@@ -5,5 +5,103 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Reader {
-	
+
+	private Elimination elimination;
+
+	public Reader(String filename) throws IOException {
+
+		// Create object of FileReader
+		FileReader inputFile = new FileReader(filename);
+		FileReader inputFile2 = new FileReader(filename);
+
+		System.out.println("invoke buffered reader");
+		BufferedReader reader = new BufferedReader(inputFile);
+
+		// reader2 is to determine the number of elimination variables,
+		// variables and conditions
+		BufferedReader reader2 = new BufferedReader(inputFile);
+
+		String line;
+		System.out.println("start loop to read in single lines");
+
+		/**
+		 * At first we determine how many variable we have to delete We found
+		 * them in the first line.
+		 */
+
+		// read in the first line
+		line = reader2.readLine();
+		String[] arr = line.split(" ");
+
+		// number of variables in the first line
+		int numberOfEliminationVariables = arr.length;
+
+		/**
+		 * Then we determine the number of variables in the problem. We found
+		 * them in the second line.
+		 */
+
+		// read in second line
+		line = reader2.readLine();
+		arr = line.split(" ");
+
+		// number of variables in the second line including 1 place for b.
+		int numberOfAllVariables = arr.length;
+
+		// number of conditions is now 1
+		int numberOfConditions = 1;
+
+		/**
+		 * Now we determine the number of conditions
+		 */
+		while ((line = reader2.readLine()) != null) {
+			numberOfConditions++;
+		}
+
+		// create an array with variables to eliminate
+		int[] eliminationVariables = new int[numberOfEliminationVariables];
+
+		// create the matrix A with the coefficients. In the last column will be
+		// the vector b
+		int[][] matrixA = new int[numberOfConditions][numberOfAllVariables];
+
+		/**
+		 * Here comes the read in part.
+		 * 
+		 * At First we put all variables we have to eliminate in the array
+		 */
+
+		// read first line
+		line = reader.readLine();
+		arr = line.split(" ");
+
+		// put all elimination variables in an array
+		for (int i = 0; i < numberOfEliminationVariables; i++) {
+			eliminationVariables[i] = Integer.parseInt(arr[i]);
+		}
+
+		/**
+		 * Now put read every condition line and put all coefficients and b in a
+		 * matrix To keep it easy, the first line and the first column will be
+		 * 0.
+		 */
+		int j = 1; // the row index starts at 1
+		while ((line = reader.readLine()) != null) {
+			arr = line.split(" ");
+			for (int i = 0; i < arr.length; i++) {
+				matrixA[j][i + 1] = Integer.parseInt(arr[i]); // put coefficient
+																// in the matrix
+																// A
+			}
+			j++; // next row
+		}
+
+//		Elimination elimination = new Elimination(matrixA, eliminationVariables);
+
+	}
+
+	public Elimination getElimination() {
+		return this.elimination;
+	}
+
 }
